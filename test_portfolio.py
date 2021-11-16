bfa02e908bfb21d89f407b4bcf0d08212f73cd4e
tinglei11 <93166747+tinglei11@users.noreply.github.com>
2021-11-16 16:58:13 -0500

    adds some tests for the portfolio
    uses pytes

---
+++ test_portfolio.py
@@ -0,0 +1,18 @@
+from portfolio import Portfolio
+
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost() == 0.0
+
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
+
+def test_buy_two_stocks():
+    p = Portfolio()
+    p.buy ("IBM" , 100, 176.48)
+    p.buy ("HPQ", 100, 36.15)
+    assert p.cost() == 21263.00
+ 
+ 
